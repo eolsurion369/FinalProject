b27a7da846852e4036c1e19eea0125a109d43b62
eolsurion369 <eolsurion369@hotmail.com>
2017-12-06 07:29:39 -0800

    FrontEndCreation
    Creation of Front End, modification of project to effect web app changes for LIbrary project.  Incorporation of EF with reverse engineering to existing database.

--- Database/createMemberAddress.sql
+++ Database/createMemberAddress.sql
@@ -1,5 +1,6 @@
 CREATE TABLE AdultMemberAddress (
 MemberID int NOT NULL,
 AddressID int NOT NULL,
+CONSTRAINT PK_AdultMemberAddress PRIMARY KEY (MemberID, AddressID),
 CONSTRAINT FK_MemberAddress_AdultMember FOREIGN KEY (MemberID) REFERENCES AdultMember(ID),
 CONSTRAINT FK_MemberAddress_Address FOREIGN KEY (AddressID) REFERENCES [Address](ID))
---
+++ FrontEnd/NW_Central_Library/Models/AdultMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NW_Central_Library.Models
+{
+    public partial class AdultMember
+    {
+        public AdultMember()
+        {
+            AdultMemberAddress = new HashSet<AdultMemberAddress>();
+            CheckOut = new HashSet<CheckOut>();
+            JuvenileMember = new HashSet<JuvenileMember>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MidInit { get; set; }
+        public string Suffix { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string PrimaryPhone { get; set; }
+        public string EmailAddress { get; set; }
+
+        public ICollection<AdultMemberAddress> AdultMemberAddress { get; set; }
+        public ICollection<CheckOut> CheckOut { get; set; }
+        public ICollection<JuvenileMember> JuvenileMember { get; set; }
+    }
+}
